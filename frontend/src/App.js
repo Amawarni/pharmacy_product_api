9f8f0744defab0afb0586812c1ff8114f471431b
Amawarni <as2018560@sci.sjp.ac.lk>
2021-10-11 02:09:51 +0530

    first part of frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CounterClass from './component/CounterClass';
+import CounterFunction from './component/CounterFunction';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+     
+    </div>
+  );
+}
+
+export default App;
